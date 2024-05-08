7415b404d0270591f4999486df8ddd1386bed7b1
nguyen <ngoquangnguyen2504@gmail.com>
2024-05-08 10:29:30 +0700

    upload part 1

---
+++ library.js
@@ -0,0 +1,10 @@
+const arrowBtns = document.querySelectorAll(".thuvien .arrow");
+const carousel = document.querySelector(".thuvien .carousel");
+const cards = document.querySelectorAll(".thuvien .card");
+const firstCardWidth = document.querySelector(".thuvien .card").offsetWidth;
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? - 230 : 230;
+    })
+})
